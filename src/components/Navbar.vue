4ef86314baea429b0356777089814638960ab72a
Touha Zohair <touha98@gmail.com>
2020-06-01 22:02:24 +0600

    style changed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,10 @@
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
           <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button class="my-2 my-sm-0" variant="info" type="submit"
+          <b-button
+            class="my-2 my-sm-0"
+            variant="outline-secondary"
+            type="submit"
             >Search</b-button
           >
         </b-nav-form>
